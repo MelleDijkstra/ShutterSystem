2fed048282500eaf5f659e24568dd12cf851435f
MelleDijkstra <melle210202@gmail.com>
2017-10-20 17:58:22 +0200

    init commit

---
+++ ShutterSystem/main.c
@@ -0,0 +1,18 @@
+/*
+ * ShutterSystem.c
+ *
+ * Created: 20-10-2017 17:41:13
+ * Author : melle
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
