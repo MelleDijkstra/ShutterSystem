fb76d2f2bb439c20f50048bd46db608c61e3610f
MelleDijkstra <melle210202@gmail.com>
2017-10-25 10:17:35 +0200

    [WIP] Reading temperature sensor and send with serial communication

--- ShutterSystem/tempsensor.c
+++ ShutterSystem/tempsensor.c
@@ -4,3 +4,37 @@
  * Created: 24-10-2017 13:26:16
  *  Author: melle
  */ 
+
+ #include "analog/analog.h"
+ #include "serial/serialconnection.h"
+ #include <avr/io.h>
+ #define F_CPU 16E6
+ #include <util/delay.h>
+
+ int main (void)
+ {
+	initADC();
+	initUART();
+
+	while(1)
+	{
+
+		uint8_t adc_value = ~readADC(0); // read A0 pin and invert the result
+
+		transmit(adc_value);
+		// mV * 5v
+		float voltage = adc_value * 5.0;
+		//convert mV to Voltage
+		voltage /= 1024.0;
+
+		//voltage - 500mV
+		//to celcius * 100
+		//uint8_t cTemperature = (getVoltage() - 0.5) * 100;
+		uint8_t temperature = (voltage - 0.5) * 100;
+
+		transmit(temperature);
+
+		_delay_ms(1000);
+	}
+
+ }
